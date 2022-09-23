c003ffa2c254a8dc3d6af7788193fccb211c2ea2
henryposa <luisgasthof@hotmail.com>
2022-09-22 19:35:59 -0500

    primer commit con titulo en app

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// 3 todo el componente
+import { useState } from 'react'
+// 5 
+import { Container } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <>
+      {/* Padding vertical arriba y abajo */}
+      <header className='py-5'>
+        <h1>Buscador de bebidas</h1>
+      </header>
+
+      {/* 6 */}
+      <Container className="mt-5">
+        1
+      </Container>
+
+    </>
+  )
+}
+
+export default App
