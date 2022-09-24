3b413632f6ccc33a1a0de76e9c095a3e1cd7b19d
henryposa <luisgasthof@hotmail.com>
2022-09-23 23:24:53 -0500

    iterando sobre categorias

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,6 +1,11 @@
 import { Button, Form, Row, Col } from 'react-bootstrap'
+// 4 -
+import useCategorias from '../hooks/useCategorias'
 
 const Formulario = () => {
+    // 5 - 
+    const { categorias } = useCategorias()
+
     return (
         <Form>
             <Row>
@@ -23,14 +28,37 @@ const Formulario = () => {
                     <Form.Group className="mb-3">
                         {/* htmlFor al dar click en el label pone un focus en el input
                         se coloca el id="nombre" en el input */}
-                        <Form.Label htmlFor="nombre">Categoria bebida</Form.Label>
+                        <Form.Label htmlFor="categoria">Categoria bebida</Form.Label>
                         {/* input */}
                         <Form.Select id="categoria" name="categoria">
                             <option>- Selecciona categoria</option>
+                            {/* 6 - */}
+                            {categorias.map(categoria => (
+                                <option
+                                    key={categoria.strCategory}
+                                    value={categoria.strCategory}
+                                >{categoria.strCategory}
+                                </option>
+                            ))}
+                            {/* // */}
                         </Form.Select>
                     </Form.Group>
                 </Col>
             </Row>
+            {/* 7  justify coloca el boton al final*/}
+            <Row className='justify-content-end'>
+                {/* Toma 3 de las 12 columnas */}
+                <Col md={3}>
+                    <Button
+                    variant='danger'
+                    // w-100 ocupa todo el ancho del md-3
+                    className='text-uppercase w-100'
+                    >
+                        Buscar bebidas
+                    </Button>
+                </Col>
+            </Row>
+            {/* // */}
         </Form>
     )
 }
