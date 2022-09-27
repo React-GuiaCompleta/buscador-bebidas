93848570478ba594a89ac56356f89dfcf0e669b2
henryposa <luisgasthof@hotmail.com>
2022-09-27 16:41:52 -0500

    Agregando card a las bebidas

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,7 +1,30 @@
+// 1 
+import { Col, Card, Button } from "react-bootstrap"
+
 // extraemos prop de bebida
 const Bebida = ({ bebida }) => {
     return (
-        <div>{bebida.strDrink}</div>
+        // 6 columnas, 4 columnas de las 12 columnas
+        <Col md={6} lg={4}>
+            <Card className="mb-4">
+                <Card.Img
+                    variant='top'
+                    src={bebida.strDrinkThumb}
+                    alt={`Imagen de ${bebida.strDrink}`}
+                />
+
+                <Card.Body>
+                    <Card.Title>{bebida.strDrink}</Card.Title>
+                    <Button
+                        variant={'warning'}
+                        className="w-100 text-uppercase mt-2"
+                    >
+                        Ver receta
+                    </Button>
+                </Card.Body>
+                <Card.Text>Algo más</Card.Text>
+            </Card>
+        </Col>
     )
 }
 
